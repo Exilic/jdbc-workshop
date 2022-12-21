f970bc648b260c78221c6a7736fa8c0d662f6ebe
Exilic <tobias.engberg@exilic.se>
2022-12-21 14:26:22 +0100

    Models templated

---
+++ src/main/java/org/example/models/CityDaoJDBC.java
@@ -0,0 +1,40 @@
+package org.example.models;
+
+import java.util.List;
+
+public class CityDaoJDBC implements CityDao{
+    @Override
+    public City findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByCode(String code) {
+        return null;
+    }
+
+    @Override
+    public List<City> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<City> findAll() {
+        return null;
+    }
+
+    @Override
+    public City add(City city) {
+        return null;
+    }
+
+    @Override
+    public City update(City city) {
+        return null;
+    }
+
+    @Override
+    public int delete(City city) {
+        return 0;
+    }
+}
